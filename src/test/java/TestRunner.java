6fb6ef282962f629e714633a658e5af1536fe42e
kakuffo <kakuffo@curoonline.com>
2015-09-17 00:18:32 +0100

    project

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,15 @@
+/**
+ * Created by kakuffo on 17/09/15.
+ */
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(format = {"pretty", "html:target/cucumber"},
+        features = "src/test/java/com/whiteleys/zoo/features")
+public class TestRunner {
+
+}
