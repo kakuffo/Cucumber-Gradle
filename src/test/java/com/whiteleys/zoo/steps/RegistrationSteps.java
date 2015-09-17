d1c09da181c3dbb55c749f6b7304794f0e2738b6
kakuffo <kakuffo@curoonline.com>
2015-09-17 13:39:18 +0100

    added RegistrationSteps class without import

---
+++ src/test/java/com/whiteleys/zoo/steps/RegistrationSteps.java
@@ -0,0 +1,29 @@
+package com.whiteleys.zoo.steps;
+
+/**
+ * Created by kakuffo on 17/09/15.
+ */
+public class RegistrationSteps {
+
+    // You can implement missing steps with the snippets below:
+
+    @Given("^User has entered valid data for all fields$")
+    public void user_has_entered_valid_data_for_all_fields() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User press the Register button$")
+    public void user_press_the_Register_button() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^User is forwarded to home\\.html view/page$")
+    public void user_is_forwarded_to_home_html_view_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+
+    }
+
+}
