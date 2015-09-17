05be33a1a0f36493f5e0b83dbf4b1c34c306fe9b
kakuffo <kakuffo@curoonline.com>
2015-09-17 12:47:59 +0100

    added   features = "src/test/java/com/whiteleys/zoo/features")

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(format = {"pretty", "html:target/cucumber"},
-        features = "src/test/resources/features")
+        features = "src/test/java/com/whiteleys/zoo/features")
 public class TestRunner {
 
 }
